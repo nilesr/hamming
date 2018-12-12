48c88d875191511fe734056e9057aa7cda1db9df
Niles Rogoff <me@niles.xyz>
2018-12-12 18:00:13 -0500

    add master support

--- hamming.py
+++ hamming.py
@@ -1,6 +1,7 @@
 import math, random, sys, base64
 length = int(sys.argv[1])
 use_lines = int(sys.argv[2]) == 1
+master = int(sys.argv[3]) == 1
 lines = []
 def isparity(pos):
     return pos.count("1") == 1
@@ -8,7 +9,7 @@ blen = math.ceil(math.log2(length)) # 4
 bpos = [bin(length - i)[2:].rjust(blen, "0") for i in range(length)]
 ppos = [isparity(x) for x in bpos]
 slen = ppos.count(False) # 7
-lines.append("{},{} code".format(length, slen))
+lines.append("{},{} code".format(length + master, slen))
 bits = []
 # generate bits or parity bits
 for i in range(length):
@@ -23,8 +24,20 @@ for i in range(length):
         bits.append(parity)
     else:
         bits.append(random.randint(0, 1))
+if master:
+    length += 1
+    ppos.append(True)
+    count = 0
+    for i in bits:
+        count += i
+    parity = count & 1
+    bpos.append("0" * blen)
+    bits.append(parity)
 # fuck with it
 bits[random.randint(0, length - 1)] = random.randint(0, 1)
+if master:
+    bits[random.randint(0, length - 1)] = random.randint(0, 1)
+
 # print out bits
 lines.append(" ".join(bpos))
 lines.append(" ".join([str(x).rjust(blen, " ") for x in bits]))
@@ -34,8 +47,10 @@ if use_lines:
         if not ppos[i]: continue
         inline = False
         guarded_bit = bpos[i].find("1")
+        if guarded_bit == -1: guarded_bit = blen - 1
+        is_master = master and i == length - 1
         for j in range(i):
-            if bpos[j][guarded_bit] == "1":
+            if bpos[j][guarded_bit] == "1" or is_master:
                 pc = "─" if inline else " "
                 uc = "┴" if inline else "└"
                 for i in range(guarded_bit):
@@ -60,19 +75,33 @@ for i in range(length):
     guarded_bit = bpos[i].find("1")
     count = 0
     for j in range(i):
-        if bpos[j][guarded_bit] == "1":
+        if bpos[j][guarded_bit] == "1" or (master and i == length - 1):
             count += bits[j]
     parity = count & 1
     valid = parity == bits[i]
     if not valid:
         invalid.append(i)
-#print(invalid)
-if len(invalid) > 0:
-    pos = 0
-    for invpos in invalid:
-        pos |= int(bpos[invpos], 2)
-    lines.append("Bit in position " + bin(pos)[2:].rjust(blen, "0") + " is invalid")
+lines.append(str(invalid))
+pos = 0
+for invpos in invalid:
+    pos |= int(bpos[invpos], 2)
+#lines.append("Bit in position " + bin(pos)[2:].rjust(blen, "0") + " is invalid")
+if master:
+    if length - 1 in invalid:
+        # master bit was incorrect
+        if len(invalid) == 1:
+            lines.append("Master bit in position " + bin(pos)[2:].rjust(blen, "0") + " is invalid")
+        else:
+            lines.append("Bit in position " + bin(pos)[2:].rjust(blen, "0") + " is invalid")
+    else:
+        if len(invalid) > 0:
+            lines.append("Two-bit error -- uncorrectable")
+        else:
+            lines.append("All bits were valid")
 else:
-    lines.append("All bits were valid")
+    if len(invalid) > 0:
+        lines.append("Bit in position " + bin(pos)[2:].rjust(blen, "0") + " is invalid")
+    else:
+        lines.append("All bits were valid")
 lines = "\n".join(lines)
 print(base64.b64encode(lines.encode("utf-8")).decode("ascii")) # python 2 compat hackery
